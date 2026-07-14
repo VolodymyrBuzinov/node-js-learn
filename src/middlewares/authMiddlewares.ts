82654dc85d892185322b04fb426f8c5bc7677903
VolodymyrBuzinov <vova.buzz@gmail.com>
2026-07-14 13:58:25 +0300

    fix auth middleware

--- src/middlewares/authMiddlewares.ts
+++ src/middlewares/authMiddlewares.ts
@@ -1,8 +1,8 @@
 import { HTTP_STATUS_CODES } from "@/config/consts.js";
 import { userClient } from "@/config/supabase.js";
 import { AppError } from "@/services/appError.js";
+import { asyncHandler } from "@/utils/asyncHandler.js";
 import { NextFunction, Request, Response } from "express";
-import { getUserByIdService } from "@/modules/user/userService.js";
 
 const getToken = (req: Request) => {
   const token = req.cookies?.accessToken;
@@ -28,25 +28,24 @@ const getUser = async (token: string) => {
   return data.user.id;
 };
 
-export const adminAuthMiddleware = async (
+export const adminAuthMiddleware = asyncHandler(async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const token = await getToken(req);
+  const token = getToken(req);
   const userId = await getUser(token);
   res.locals.auth = { userId, role: "admin" };
   next();
-};
+});
 
-export const userAuthMiddleware = async (
+export const userAuthMiddleware = asyncHandler(async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const token = await getToken(req);
+  const token = getToken(req);
   const userId = await getUser(token);
-  await getUserByIdService(userId);
   res.locals.auth = { userId, role: "user" };
   next();
-};
+});
